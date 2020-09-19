acceac47204ce11e33a115a75b3ad627f684b05f
Marta Sousa <msousa@4dmais.com.br>
2020-09-19 18:03:48 -0300

    converting app to redux toolkit

--- src/index.js
+++ src/index.js
@@ -1,18 +1,21 @@
 import React from 'react'
-import {render} from 'react-dom'
-import {createStore, applyMiddleware} from 'redux'
-import {Provider} from 'react-redux'
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
+import { render } from 'react-dom'
+import { configureStore, ConfigureStore } from '@reduxjs/toolkit' //new
+import { Provider } from 'react-redux'
+//import {createStore, applyMiddleware} from 'redux'
+//import thunk from 'redux-thunk'
+//import {composeWithDevTools} from 'redux-devtools-extension'
 
 // Local imports
 import App from './App'
-import rootReducer from './reducers'
+import rootReducer from './slices' //new
+//import rootReducer from './reducers'
 
 // Assets
 import './index.css'
 
-const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
+const store = configureStore({ reducer: rootReducer })
+//const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
 
 render(
   <Provider store={store}>
--- src/pages/PostsPage.js
+++ src/pages/PostsPage.js
@@ -1,12 +1,20 @@
 import React, { useEffect } from 'react'
-import { connect } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux' //new
+import { fetchPosts, postsSelector } from "../slices/posts"; //new
+// import { connect } from 'react-redux'
 
 // Bring in the asynchronous fetchPosts action
-import { fetchPosts } from '../actions/postsActions'
+//import { fetchPosts } from '../actions/postsActions'
+
 import { Post } from '../components/Post'
 
 // Redux state is now in the props of the component
-const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
+//const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
+
+const PostsPage = () => {
+    const dispatch = useDispatch() //new
+    const {posts, loading, hasErrors} = useSelector(postsSelector)
+
     useEffect(() => {
         dispatch(fetchPosts())
     }, [dispatch])
@@ -15,6 +23,7 @@ const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
     const renderPosts = () => {
         if (loading) return <p>Loading posts...</p>
         if (hasErrors) return <p>Unable to display posts.</p>
+
         return posts.map((post) => <Post key={post.id} post={post} />)
     }
 
@@ -26,15 +35,25 @@ const PostsPage = ({ dispatch, loading, posts, hasErrors }) => {
     )
 }
 
-// Map Redux state to React component props
+export default PostsPage
+
+
+
+
+
+//OLD WAY
+
+// Map Redux state to React component props (longer way)
 //   const mapStateToProps = (state) => ({
 //     loading: state.posts.loading,
 //     posts: state.posts.posts,
 //     hasErrors: state.posts.hasErrors,
 //   })
-// Map Redux state to React component props
-const mapStateToProps = state => state.posts;
+
+
+// Map Redux state to React component props (short way)
+//const mapStateToProps = state => state.posts;
 
 
 // Connect Redux to React
-export default connect(mapStateToProps)(PostsPage)
+//export default connect(mapStateToProps)(PostsPage)
